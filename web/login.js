0320fda54fe39d0127d8f2e598fad0426c88a9f4
Andelo-p <andelopodrug6@gmail.com>
2022-03-26 15:03:32 +0100

    Log in part 2

--- web/login.js
+++ web/login.js
@@ -1,3 +1,21 @@
-function UserLogIn{
-    
+
+function User(username,email,password){
+    this.username=username;
+    this.email=email;
+    this.password=password;
+}
+let admin1Username=Adndelo;
+let admin1Email=admin1;
+let admin1Password=12341234;
+
+
+
+function signUp(){
+    let signInUserName=document.getElementById("signupusername").value;
+    let signInEmail=document.getElementById("signupemail").value;
+    let signInUPassword=document.getElementById("signuppassword").value;
+    let admin1=new User(signInUserName,signInEmail,signInUPassword);
+    alert("Username is :"+admin1.username);
+    alert("Email is :"+admin1.email);
+    alert("Password is :"+admin1.password);
 }
