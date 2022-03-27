181f24e28a49eda96a592ed40f391dd0bbf6f3fa
Andelo-p <andelopodrug6@gmail.com>
2022-03-28 01:21:17 +0200

    LogIn js part One

--- web/login.js
+++ web/login.js
@@ -1,21 +1,29 @@
-
-function User(username,email,password){
-    this.username=username;
-    this.email=email;
-    this.password=password;
+console.log("Start");
+function createUser(username,email,password){
+    this.username=username,
+    this.email=email,
+    this.password=password
 }
-let admin1Username=Adndelo;
-let admin1Email=admin1;
-let admin1Password=12341234;
+let admin1 = new createUser("Andelo","adminandelo@gmail.com","12345678");
+let admin2 = new createUser("Lucija","adminLucija@gmail.com","12345678");
+let allUsers=[];
+allUsers.push(admin1);
+allUsers.push(admin2);
+console.log("Phase One over");
 
 
 
-function signUp(){
-    let signInUserName=document.getElementById("signupusername").value;
-    let signInEmail=document.getElementById("signupemail").value;
-    let signInUPassword=document.getElementById("signuppassword").value;
-    let admin1=new User(signInUserName,signInEmail,signInUPassword);
-    alert("Username is :"+admin1.username);
-    alert("Email is :"+admin1.email);
-    alert("Password is :"+admin1.password);
+
+function logIn(){
+    let logInEmail = document.getElementById("loginemail");
+    let logInPassword = document.getElementById("loginpassword");
+}
+    
+
+
+
+
+
+function logInEmailCheck(){
+
 }
